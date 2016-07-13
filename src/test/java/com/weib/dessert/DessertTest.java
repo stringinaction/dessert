9dbacd4fe2540e71708f56d40117d02fa5b0fee1
scarecroweib <scarecroweib@gmail.com>
2016-07-13 16:45:46 +0800

    补充说明

--- src/test/java/com/weib/dessert/DessertTest.java
+++ src/test/java/com/weib/dessert/DessertTest.java
@@ -13,30 +13,33 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
  * @author zhangjingwei
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=DessertConfig.class)
-@ActiveProfiles("dev")
-public class DessertTest extends AbstractJUnit4SpringContextTests {
+public class DessertTest {
     
-    @Test
-    public void testProfile(){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DessertConfig.class);
+    @RunWith(SpringJUnit4ClassRunner.class)
+    @ContextConfiguration(classes=DessertConfig.class)
+    @ActiveProfiles("dev")
+    public static class DessertConfigTest{
         
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        //ctx.getEnvironment().addActiveProfile("dev");
-        String[] ap = ctx.getEnvironment().getActiveProfiles();
-        System.out.println("###################");
-        for(String s : ap){
-            System.out.println(s);
+        @Test
+        public void testProfile(){
+            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DessertConfig.class);
+
+            GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+            //ctx.getEnvironment().addActiveProfile("dev");
+            String[] ap = ctx.getEnvironment().getActiveProfiles();
+            System.out.println("###################");
+            for(String s : ap){
+                System.out.println(s);
+            }
+            ProfileBean profileBean = (ProfileBean) context.getBean(ProfileBean.class);
+            profileBean.output();
         }
-        ProfileBean profileBean = (ProfileBean) context.getBean(ProfileBean.class);
-        profileBean.output();
     }
+    
 }
